99ca5c63dcabe251b78ea4ee572dddaf6bf2b3dd
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-03-17 16:11:31 +0300

    5 test passed

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,39 @@
+# lib/stack.py
 class Stack:
+    def __init__(self, items=[], limit=100):
+        self.items = items
+        self.limit = limit
 
-    def __init__(self, items = [], limit = 100):
-        pass
-
-    def isEmpty(self):
-        pass
+    def is_empty(self):
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            raise IndexError("push to full stack")
 
     def pop(self):
-        pass
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("pop from empty stack")
 
     def peek(self):
-        pass
-    
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("peek from empty stack")
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            index = len(self.items) - self.items[::-1].index(target) - 1
+            return len(self.items) - 1 - index
+        except ValueError:
+            return -1
